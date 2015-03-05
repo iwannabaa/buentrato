0e0c26c8a6ae239919773bf3408dc47ed5f86d06
Carlos Jovel <cm.jovel@gmail.com>
2015-03-05 11:23:26 -0600

    styling trivia

--- app/scripts/controllers/trivia-item.js
+++ app/scripts/controllers/trivia-item.js
@@ -3,8 +3,10 @@
 angular.module('buentratoApp')
   .controller('TriviaItemCtrl', function ($scope, Data, $routeParams) {
   	var triviaLevel = $routeParams.id;
+  	
     $scope.trivia = Data.getTrivia(triviaLevel);
-    $scope.selectAnswer = function(index) {
-    	return index;
+
+    $scope.selectAnswer = function(questionIndex, answerIndex) {
+    	$scope.trivia.questions[questionIndex].selectedAnswer = answerIndex;
     };
   });
