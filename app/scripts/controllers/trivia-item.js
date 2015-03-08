ac2cc7311aad3b772e6631f26f95792203449ec8
Carlos Jovel <cm.jovel@gmail.com>
2015-03-08 12:44:06 -0600

    add transitions to trivia

--- app/scripts/controllers/trivia-item.js
+++ app/scripts/controllers/trivia-item.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('buentratoApp')
-  .controller('TriviaItemCtrl', function ($scope, Data, $routeParams) {
+  .controller('TriviaItemCtrl', function ($scope, Data, $routeParams, $timeout) {
     // Page setup
     $scope.pageClass = 'page-trivia-item';
     $scope.$parent.showNav = true;
@@ -10,7 +10,34 @@ angular.module('buentratoApp')
     $scope.$parent.backPage = 'trivia';
     
     $scope.trivia = Data.getTrivia($routeParams.id);
+    $scope.index = 0;
+    $scope.$questionContainer = $('.question-container');
+    $scope.$watch('index', function (newVal) {
+        if ( 'number' === typeof newVal ) {
+            $scope.question = $scope.trivia.questions[newVal];
+        }
+    });
+     $scope.next = function () {
+        if ( $scope.index < $scope.trivia.questions.length - 1 ) {
+            $scope.$questionContainer.animate({
+                opacity: 0,
+                left: -100
+            }, function () {
+                $scope.index += 1;
+                $scope.$apply();
+                $scope.$questionContainer.css({
+                    left: 100
+                }).animate({
+                    opacity: 1,
+                    left: 0
+                });
+            });
+        }
+    };
     $scope.selectAnswer = function(questionIndex, answerIndex) {
-    	$scope.trivia.questions[questionIndex].selectedAnswer = answerIndex;
+    	$scope.question.selectedAnswer = answerIndex;
+        $timeout( function(){ 
+            $scope.next();
+        }, 100);
     };
   });
